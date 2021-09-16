fafc29249fd3839525c2b053f52e292045b7e197
Qiu_Zeyuan <Qiu_zeyuan@126.com>
2021-09-16 19:40:23 +0800

    master

--- keyCount.py
+++ keyCount.py
@@ -28,27 +28,31 @@ if_elif_else_count = 0
 if_stack=[]
 
 # 设定关键词字
-KEYWORDS = [
+KEYWORDS = (
     'auto', 'break', 'case', 'char', 'const', 'continue','default',
     'do', 'double', 'else', 'enum', 'extern','float', 'for', 'goto',
     'if', 'int', 'long', 'register', 'return', 'short', 'signed',
     'sizeof', 'static','struct', 'switch', 'typedef', 'union', 
     'unsigned', 'void', 'volatile', 'while'
-]
+)
 
 # 文件预处理函数
 def data_pretreatment(data_path):
-    data = open(data_path, mode='r').read()
-    # 将所有空白字符及长串空格都改为一个空格
-    # 防止有人打一堆无用空格
-    data = re.sub(r"\s +", " ", data)
-    # 通过正则表达式并将提取单词
-    # 正则表达式"\W"可提取出单词数字及下划线
-    data = re.split(r"\W", data)
-    return data
+    """
+    	传入 文件路径参数 data_path
+	    返回 经预处理列表 data_listed
+	"""
+    data = open(data_path, mode='r').read() # 读取 data_path 指向文件
+    data_shorted = re.sub(r"\s +", " ", data)    # 缩减冗余不可见字符
+    data_listed = re.split(r"\W", data_shorted)  # 获取单词列表
+    return data_listed  # 返回经过预处理的 data_listed
 
 # 关键字处理函数
-def keyword_process(data):
+def keyword_process(data_listed):
+    """
+        输入 待处理列表 data_listed
+        返回 无
+    """
 
     #使用全局变量声明
     global key_count
@@ -58,83 +62,69 @@ def keyword_process(data):
     global if_elif_else_count
     global if_stack
 
-    # 逐个单词排查是否出现于关键词字典中
-    data_iter = iter(range(len(data)))
+    data_iter = iter(range(len(data_listed)))   # 生成数据列表迭代器
     for i in data_iter:
-        # 判断是否为关键词
-        word = data[i]
-        if word != '' and word in KEYWORDS:
-
-            # 是关键词则 key_count++
-            key_count = key_count + 1
-
-            # 若为 switch 则 switch_count++ 
-            # 且 case_count 增加一个元素
-            if word == 'switch':
-                switch_count = switch_count + 1
-                case_count.append(0)
-
-            # 若是 case 则 case_count 对应元素自增
-            elif word == 'case':
-                case_count[switch_count-1] = case_count[switch_count-1] + 1
         
-            # 若是 if 则压入堆栈
-            elif word == 'if':
+        word = data_listed[i]
+
+        if word != '' and word in KEYWORDS:  # 判断是否为关键词
+
+            key_count = key_count + 1   # 是关键词则 key_count++
+
+            if word == 'switch':        # 若为 switch 则 switch_count++ 
+                switch_count = switch_count + 1 
+                case_count.append(0)    # 且 case_count 增加一个元素
+
+            
+            elif word == 'case':        # 若是 case
+                case_count[-1] += 1     # case_count 对应元素加1
+        
+           
+            elif word == 'if':          # 若是 if 则压入堆栈
                 if_stack.append('if')
 
-            # 若出现 else
-            elif word == 'else' :
+            elif word == 'else' :       # 若出现 else
 
-                # 判断是不是 else if
-                if data[i+1] == 'if':
-
-                    # 若是则将 elif 压栈删除下一个 if
-                    if_stack.append('elif')
-                    key_count += 1
-                    data_iter.__next__()
+                if data_listed[i+1] == 'if':    # 若是 elseif
+                    if_stack.append('elif')     # 压栈 elif
+                    key_count = key_count + 1   # 计数下一个 if 
+                    data_iter.__next__()        # 跳过下一个 if
                 
-                # 若只是 else
-                else :
+                else :                  # 若只是 else
 
-                    # 初始化 elifFlag 标志
-                    elifFlag=0
+                    elifFlag = False    # 初始化 elifFlag 标志
 
-                    # elif 出栈
                     while if_stack[-1] == "elif":
-                        elifFlag=1
-                        if_stack.pop()
-                    
-                    # 该 else 对应 if 出栈
-                    if_stack.pop()
+                        elifFlag =True
+                        if_stack.pop()  # elif 出栈
+                    if_stack.pop()      # 当前 else 对应 if 出栈
 
-                    # 根据 elifFlag 判断为 if_else 或 if_elif_else
-                    if elifFlag :
-                        if_elif_else_count = if_elif_else_count + 1
+                    if elifFlag :   # 判断为 if_else 或 if_elif_else
+                        if_elif_else_count += 1
                     else :
-                        if_else_count = if_else_count + 1
+                        if_else_count += 1
 
 # 输出函数
 def final_print(mode):
-    if(mode>=1):
+    if mode >= 1:
         print("total num:",key_count)
     if mode >= 2:
         print("switch num:",switch_count)
         print("case num:",end=' ')
         if switch_count > 0:
-                for i in range(switch_count):
-                    print(case_count[i],end=' ')
+                print(*case_count, sep=' ') 
         else:
             print(0)
     if mode >= 3:
-        print('\nif-else num:',if_else_count)
+        print('if-else num:',if_else_count)
     if mode >= 4:
         print('if-elseif-else num:',if_elif_else_count)
 
 if __name__ == "__main__":
     # 读入目标地址信息 dataPath 和检索模式 mode
     # 读入参数 文件名 dataPath 和 难度 mode
-    path, mode = sys.argv[1:3]
-    mode = int(mode)
+    path, print_mode = sys.argv[1:3]
+    print_mode = int(print_mode)
     PARAGRAPH = data_pretreatment(path) # 调用文件预处理函数
     keyword_process(PARAGRAPH)  # 调用关键字处理函数
-    final_print(mode)   # 调用结果输出函数
+    final_print(print_mode)   # 调用结果输出函数
